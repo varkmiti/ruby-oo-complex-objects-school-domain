0255c4015017f36e751c273171606c8bfdde72c4
Mark Viti <+>
2022-05-31 11:50:12 -0400

    commit tree 94931c2c600b2501342d36660254bc3eab07c368

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,37 @@
 # code here!
+require 'pry'
+
+class School
+    def initialize(name)
+        @name = name
+        @roster = Hash.new
+    end 
+
+    def roster 
+        @roster
+    end 
+
+    def add_student(name, grade)
+        if @roster[grade] == nil 
+            @roster[grade] = []
+            @roster[grade] << name
+        elsif
+            @roster[grade] << name
+        end 
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end 
+
+    def sort
+        # My squad had to help me with this; 
+        #each over a hash was a little wonky
+        sorted_roster = {}
+        @roster.each do |grade, students|
+            sorted_roster[grade] = students.sort
+        end
+        sorted_roster
+    end
+end
+
